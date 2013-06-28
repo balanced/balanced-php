b483352eb2cdc48ae33a2cd3a8af286bc036ac24
Parham Negahdar <pnegahdar@gmail.com>
2013-06-27 22:56:05 -0700

    Card and BA stuff.

---
+++ scenarios/card_delete/card_delete.php
@@ -0,0 +1,12 @@
+<?php
+
+require(__DIR__ . '/vendor/autoload.php');
+
+Httpful\Bootstrap::init();
+RESTful\Bootstrap::init();
+Balanced\Bootstrap::init();
+
+Balanced\Settings::$api_key = "2fd37702d33511e2a00f026ba7d31e6f";
+
+$card = Balanced\Card::delete("/v1/bank_accounts/BA64BU1PUa8MKCAg0omlaNwm");
+$card->delete();
