27ded9e59e52f917cb7d27e1461af81bc16b0172
bninja <andrew@poundpay.com>
2013-06-28 13:41:47 -0700

    v0.7.2 release

--- src/Balanced/Settings.php
+++ src/Balanced/Settings.php
@@ -22,7 +22,7 @@ namespace Balanced;
  */
 class Settings
 {
-    const VERSION = '0.7.1';
+    const VERSION = '0.7.2';
 
     public static $url_root = 'https://api.balancedpayments.com',
                   $api_key = null,
