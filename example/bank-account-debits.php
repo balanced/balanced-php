1ffd9a83bd47567d1656c25503fc4d34c928ada4
Ben Mills <ben@unfiniti.com>
2013-10-14 12:15:23 -0600

    Update examples

--- example/bank-account-debits.php
+++ example/bank-account-debits.php
@@ -1,6 +1,6 @@
 <?php
 //
-// Learn how to authenticate a bank account so you can debit with it.
+// Create, verify, and debit a bank account.
 //
 
 require(__DIR__ . '/vendor/autoload.php');
@@ -9,46 +9,58 @@ Httpful\Bootstrap::init();
 RESTful\Bootstrap::init();
 Balanced\Bootstrap::init();
 
-// create a new marketplace
+// Create a new marketplace
 $key = new Balanced\APIKey();
 $key->save();
 Balanced\Settings::$api_key = $key->secret;
 $marketplace = new Balanced\Marketplace();
 $marketplace->save();
 
-// create a bank account
-$bank_account = $marketplace->createBankAccount("Jack Q Merchant",
-    "123123123",
-    "123123123"
-);
-$buyer = $marketplace->createAccount("buyer@example.org");
-$buyer->addBankAccount($bank_account);
+// Create a bank account
+$bank_account = new \Balanced\BankAccount(array(
+    "account_number" => "9900000001",
+    "name" => "Johann Bernoulli",
+    "routing_number" => "121000358",
+    "type" => "checking",
+));
+$bank_account->save();
+print "The BankAccount: " . $bank_account->uri;
 
-print("you can't debit from a bank account until you verify it\n");
+// Create a Customer
+$customer = new \Balanced\Customer(array(
+  "name" => "William Henry Cavendish III",
+  "email" => "william@example.com",
+));
+$customer->save();
+
+// Add the bank account to the Customer
+$customer->addBankAccount($bank_account);
+
+print("You can't debit from a bank account until you verify it\n");
 try {
-    $buyer->debit(100);
+    $customer->debit(100);
 } catch (Exception $e) {
-    printf("Debit failed, %s\n", $e->getMessage());
+    printf("Debit failed, %s\n", $e->description);
 }
 
-// authenticate
+// Initiate a bank account verification
 $verification = $bank_account->verify();
 
+// Verify the bank account
 try {
     $verification->confirm(1, 2);
 } catch (Balanced\Errors\BankAccountVerificationFailure $e) {
-    printf('Authentication error , %s\n', $e->getMessage());
+    printf("Verification error , %s\n", $e->description);
     print("PROTIP: for TEST bank accounts the valid amount is always 1 and 1\n");
-
 }
 
 $verification->confirm(1, 1);
 
-$debit = $buyer->debit(100);
-printf("debited the bank account %s for %d cents\n",
+$debit = $customer->debit(100);
+printf("Debited the bank account %s for %d cents\n",
     $debit->source->uri,
     $debit->amount
 );
-print("and there you have it");
+
 
 ?>
