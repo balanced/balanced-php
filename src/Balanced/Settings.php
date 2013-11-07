c843db7118d1bbb309e8951daa883b9b54c891c1
Ben Mills <ben@unfiniti.com>
2013-11-07 13:46:14 -0700

    v0.7.4.1 release

--- src/Balanced/Settings.php
+++ src/Balanced/Settings.php
@@ -22,7 +22,7 @@ namespace Balanced;
  */
 class Settings
 {
-    const VERSION = '0.7.4';
+    const VERSION = '0.7.4.1';
 
     public static $url_root = 'https://api.balancedpayments.com',
                   $api_key = null,
