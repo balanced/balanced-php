753918aa9f7d8e83e52470e07855393aebb8ff88
Marshall Jones <marshall@balancedpayments.com>
2014-01-03 16:05:30 -0700

    bump version to 0.7.5.0

--- src/Balanced/Settings.php
+++ src/Balanced/Settings.php
@@ -22,7 +22,7 @@ namespace Balanced;
  */
 class Settings
 {
-    const VERSION = '0.7.4.1';
+    const VERSION = '0.7.5.0';
 
     public static $url_root = 'https://api.balancedpayments.com',
                   $api_key = null,
