77ed591530b6e50e6e36834d0f2cf23e947f5090
Richard Serna <Bigpapa1>
2014-06-10 14:20:45 -0700

    Edit templates to render nested objects

--- scenarios/customer_create/request.php
+++ scenarios/customer_create/request.php
@@ -1,6 +1,14 @@
 $marketplace = Balanced\Marketplace::mine();
 $customer = $marketplace->customers->create(array(
 {% for k, v in request.payload %}
-    "{{ k }}" => "{{ v }}",
+{% if v is iterable%}
+"{{ k }}" => array(
+{% for b, a in v %}
+    "{{ b }}" => "{{ a }}",
+{% endfor %}
+),
+{% else %}
+"{{ k }}" => "{{ v }}",
+{% endif %}
 {% endfor %}
 ));
