b830358fb5a781b2a18978e6889b4faeaa94445a
richie serna <richie@balancedpayments.com>
2014-08-20 12:45:32 -0700

    Add order-credit-marketplace snippet

---
+++ snippets/order-credit-marketplace.php
@@ -0,0 +1,6 @@
+<?php
+Balanced\Marketplace::mine()->owner_customer->bank_accounts->query()->first()->credits->create(array(
+    "amount" => "2000000",
+    "description" => "Credit from order escrow to marketplace bank account",
+));
+?>
