4110956b39e8e50067ec906f3bdf78c6b0e68d08
richie serna <richie@balancedpayments.com>
2014-08-21 14:37:44 -0700

    Add order param to credit and debit scenarios

--- snippets/card-credit.php
+++ snippets/card-credit.php
@@ -1,7 +1,9 @@
 <?php
+// $order_href is the stored href for the Order
 $card->credits->create(array(
     "amount" => "5000",
     "appears_on_statement_as" => "Some text",
     "description" => "Some descriptive text for the debit in the dashboard",
+    'order' => $order_href
 ));
 ?>
